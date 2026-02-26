d2bf3a74a1132b5495b64b1ca883f712e3dc0ff3
Dhruv1249 <dhr1249.lm@gmail.com>
2026-02-26 14:23:02 +0530

    maybe qs was the problem idk

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -4,24 +4,23 @@ const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const SERVER_URL = process.env.PROXY_URL;
 
 async function getAccessToken(code) {
+  const params = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    code: code,
+    redirect_uri: `${SERVER_URL}/api/auth/github/callback`,
+  });
+
   const response = await axios.post(
     "https://github.com/login/oauth/access_token",
-    qs.stringify({
-      client_id: process.env.GITHUB_CLIENT_ID,
-      client_secret: process.env.GITHUB_CLIENT_SECRET,
-      code: code,
-      redirect_uri: `${SERVER_URL}/api/auth/github/callback`,
-    },
+    params.toString(),
     {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Accept: "application/json",
       },
     }
-  ));
-
-  
-  
+  );
 
   return response.data.access_token;
 }
