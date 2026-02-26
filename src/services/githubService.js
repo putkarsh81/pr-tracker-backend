d0a374dd37b1a2fb75e4d1eab1b600298ebe8693
Dhruv1249 <dhr1249.lm@gmail.com>
2026-02-26 14:39:54 +0530

    maybe this fixes that

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,14 +1,10 @@
 const axios = require("axios");
-const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-const SERVER_URL = process.env.PROXY_URL;
 
 async function getAccessToken(code) {
   const params = new URLSearchParams({
     client_id: process.env.GITHUB_CLIENT_ID,
     client_secret: process.env.GITHUB_CLIENT_SECRET,
     code: code,
-    redirect_uri: `${SERVER_URL}/api/auth/github/callback`,
   });
 
   const response = await axios.post(
